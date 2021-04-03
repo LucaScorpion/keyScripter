2eca66ed773780b2a45a60cd56bde77da511cfc6
Luca Scalzotto <luca@scorpiac.com>
2021-04-03 22:30:39 +0200

    Remove old parser

--- cmd/keyScripter/main.go
+++ cmd/keyScripter/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/LucaScorpion/tas-scripter/internal/lexer"
+	"github.com/LucaScorpion/tas-scripter/internal/parser"
 	"io/ioutil"
 )
 
 func main() {
 	b, _ := ioutil.ReadFile("example.txt")
-	l := lexer.NewLexer(string(b))
-	go l.Run()
-	for t := range l.Tokens {
-		fmt.Println(t)
-	}
+	parser.Parse(string(b))
 }
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -2,67 +2,13 @@ package parser
 
 import (
 	"fmt"
-	"io"
+	"github.com/LucaScorpion/tas-scripter/internal/lexer"
 )
 
-type Parser struct {
-	scan *scanner
-}
-
-func NewParser(r io.Reader) *Parser {
-	return &Parser{
-		scan: newScanner(r),
+func Parse(input string) {
+	l := lexer.NewLexer(input)
+	go l.Run()
+	for t := range l.Tokens {
+		fmt.Println(t)
 	}
-}
-
-func (p *Parser) Parse() error {
-	var err error
-	var instructions []*Instruction
-	prevPos := &position{
-		line: 1,
-		col:  1,
-	}
-
-	for err == nil {
-		// Store the previous position.
-		prevPos = &position{
-			line: p.scan.pos.line,
-			col:  p.scan.pos.col,
-		}
-
-		// Parse the next instruction.
-		var next *Instruction
-		next, err = p.nextInstruction()
-
-		// Store the instruction.
-		if next != nil {
-			instructions = append(instructions, next)
-		}
-	}
-
-	if err == io.EOF {
-		return nil
-	}
-	return fmt.Errorf("an error occurred while parsing the script (line %d, col %d): %s", prevPos.line, prevPos.col, err)
-}
-
-func (p *Parser) nextInstruction() (*Instruction, error) {
-	line, err := p.scan.readLine()
-
-	// Check for empty lines.
-	if len(line) == 0 {
-		return nil, err
-	}
-
-	var iType = functionCall
-
-	// If the line starts with a '#', it is a comment.
-	if line[0] == '#' {
-		iType = comment
-	}
-
-	return &Instruction{
-		iType: iType,
-		raw:   line,
-	}, err
 }
