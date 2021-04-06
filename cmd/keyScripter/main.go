9435c43e300ea9d6db93bedc41f22d20f3c4b08a
Luca Scalzotto <luca@scorpiac.com>
2021-04-06 23:08:44 +0200

    Add ReadFile error handling

--- cmd/keyScripter/main.go
+++ cmd/keyScripter/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/LucaScorpion/keyScripter/internal/parser"
 	"github.com/alexflint/go-arg"
-	"io/ioutil"
 	"os"
 )
 
@@ -22,8 +21,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Read the file.
+	b, err := os.ReadFile(options.Script)
+	if err != nil {
+		if pathErr, ok := err.(*os.PathError); ok {
+			fmt.Printf("An error occurred while trying to %s", pathErr.Error())
+		} else {
+			fmt.Printf("An error occurred while trying to read the script: %s", err)
+		}
+		os.Exit(1)
+	}
+
 	// Parse the script.
-	b, _ := ioutil.ReadFile(options.Script)
 	script, err := parser.Parse(string(b))
 	if err != nil {
 		fmt.Printf("An error occurred while parsing the script: %s", err)
